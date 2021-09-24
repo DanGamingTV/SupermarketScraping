f90bffacf045871a79cd0379e75f88a9400814d3
danielhoskin <builderboydh@gmail.com>
2021-09-25 11:02:45 +1200

    Finalize ready for merge

--- comparePrices.bat
+++ comparePrices.bat
@@ -1,1 +1,4 @@
 node comparer.js
+echo "Finished comparing prices"
+cd ./data_changes
+git add -A && git commit -m "Automated Commit" --no-gpg-sign && git push origin master
--- scrapePrices.bat
+++ scrapePrices.bat
@@ -1,1 +1,4 @@
 python example.py
+echo "Finished scraping prices"
+cd ./data
+git add -A && git commit -m "Automated commit" --no-gpg-sign && git push origin master
